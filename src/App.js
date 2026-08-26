30c0cfb3e8cbc356a218e9b134d551f9466cb6ce
Zena Jun <zena.jun@gmail.com>
2026-08-26 16:19:43 -0400

    clean up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
-import { RecipeCard } from '@/config'
 
 function App() {
   const [count, setCount] = useState(0)
